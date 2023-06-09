908fe780be1663d86831a0664e8b432222a0d5b7
Elise Rosky <emrosky@mtu.edu>
2023-06-08 21:02:56 -0600

    posititve pressure box set up

---
+++ MLmW/Final_Results/p500_atm/get_data_MLmW.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# extract data from dump files using the ice_ratio.awk, running_avg.ask, find_freezing_temp.py, and make_gnuplots.sh scripts
+
+START=240
+END=225
+PRESSURE=-500
+
+for VARIABLE in 21 22 23 24 25 26 27 28 29 30
+do
+	echo $VARIABLE
+	mkdir analysis/run_$VARIABLE
+	
+	./ice_ratio.awk run_$VARIABLE/prod.water.het_freeze_$VARIABLE-$START-$END.dump > analysis/run_$VARIABLE/ice_ratio.dat
+
+	# make plot of ice ratio, raw data
+	gnuplot -e "set terminal png size 1000,600; \
+	    set output 'analysis/run_$VARIABLE/ice_ratio_raw.png'; \
+            set title 'Ice Ratio vs. Temperature, MLmW, $PRESSURE atm - raw data'; \
+            set ylabel 'N ice / N total'; \
+            set xlabel 'Temp (K)'; \
+            set style data lines; \
+            set xrange [$START:$END] reverse; \
+            set xtics $END,2; \
+            plot 'analysis/run_$VARIABLE/ice_ratio.dat' using 3:7"
+	
+	# get running average of ice_ratio
+	./running_avg.awk analysis/run_$VARIABLE/ice_ratio.dat > analysis/run_$VARIABLE/ice_ratio_smooth.dat
+
+	# plot ice ratio
+	python3 ~/Freezing_Simulations/Bulk_Water_Heterogeneous/find_freezing_temp.py analysis/run_$VARIABLE/
+
+	# plot data from log file
+	./make_gnuplots.sh $VARIABLE
+	
+
+done
+
+
